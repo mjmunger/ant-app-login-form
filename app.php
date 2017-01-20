769a2170b453f1cc2dbe25c461f676c3f63999d9
Michael Munger <michael@highpoweredhelp.com>
2017-01-20 15:37:04 -0500

    Not sure what is in here. But need to merge with master

--- app.php
+++ app.php
@@ -21,7 +21,7 @@ namespace PHPAnt\Core;
  * @subpackage   Core
  * @category     UI
  * @author       Michael <michael@highpoweredhelp.com>
- */ 
+ */
 
 
 class LoginForm extends \PHPAnt\Core\AntApp implements \PHPAnt\Core\AppInterface  {
@@ -48,7 +48,7 @@ class LoginForm extends \PHPAnt\Core\AntApp implements \PHPAnt\Core\AppInterface
      * Callback for the cli-load-grammar action, which adds commands specific to this plugin to the CLI grammar.
      * Example:
      *
-     * @return array An array of CLI grammar that will be merged with the rest of the grammar. 
+     * @return array An array of CLI grammar that will be merged with the rest of the grammar.
      * @author Michael <michael@highpoweredhelp.com>
      **/
 
@@ -56,7 +56,7 @@ class LoginForm extends \PHPAnt\Core\AntApp implements \PHPAnt\Core\AppInterface
         $grammar = [];
 
         $this->loaded = true;
-        
+
         $results['grammar'] = $grammar;
         $results['success'] = true;
         return $results;
@@ -64,7 +64,7 @@ class LoginForm extends \PHPAnt\Core\AntApp implements \PHPAnt\Core\AppInterface
 
     //Uncomment this function and the following function to enable the autoloader for this plugin.
     function LoginFormAutoLoader() {
-        //REGISTER THE AUTOLOADER! This has to be done first thing! 
+        //REGISTER THE AUTOLOADER! This has to be done first thing!
         spl_autoload_register(array($this,'loadLoginFormClasses'));
         return ['success' => true];
 
@@ -96,7 +96,7 @@ class LoginForm extends \PHPAnt\Core\AntApp implements \PHPAnt\Core\AppInterface
         }
         return ['success' => true];
     }
-    
+
     /**
      * Callback function that prints to the CLI during cli-init to show this plugin has loaded.
      * Example:
@@ -118,12 +118,15 @@ class LoginForm extends \PHPAnt\Core\AntApp implements \PHPAnt\Core\AppInterface
     }
 
     function loginUser($args) {
+        $AppEngine = $args['AE'];
         include(__DIR__ . '/resources/login.php');
-        return ['success' => true];
+        return ['success' => true
+               ,'exit'    => true
+               ];
     }
 
     function logOutUser($args) {
-        
+
         //Remove the token (if present) from the database.
         $CredentialStorage = new \PHPAnt\Authentication\CredentialStorage($args['AE']->Configs->pdo  //PDO instance
                                                                          ,0                          //User's ID
--- resources/login.php
+++ resources/login.php
@@ -1,13 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!--CSS Style Sheets-->
+  <?php $AppEngine->runActions('header-inject-css'); ?>
+  <!-- End CSS Style Sheets -->
+
+  <!-- Injected Javascript / jQuery -->
+  <?php $AppEngine->runActions('header-inject-js'); ?>
+  <!-- End Javascript / jQuery -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php $configs = $AppEngine->Configs->getConfigs(['siteTitle']); echo $configs['siteTitle']; ?></title>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<style>
+form label.checkbox {
+    margin-left: 1.5%;
+}
+</style>
+</head>
+<body>
 <div class="container">
   <p class="text-center"><!-- Cord to get site logo could go here: --></p>
   <form class="form-signin" role="form" method="post">
     <h2 class="form-signin-heading text-center">Please sign in</h2>
-    <input type="text" class="form-control" name="username" id="username" placeholder="Email address" required autofocus>
+    <input type="text" class="form-control" name="username" id="username" placeholder="Network logon (i.e. john)" required autofocus>
     <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
     <label class="checkbox">
       <input type="checkbox" value="remember-me" name="remember" id="remember" checked> Remember me
     </label>
     <button class="btn btn-lg btn-andretti btn-block" type="submit">Sign in</button>
   </form>
-  <p align="center"><a href="reset.php">Forgot Your Password?</a></p>
+  <br>
+  <p align="center"><b>Login doesn't work?</b></p>
+  <p align="center"> Make sure you're using the same username and password you use to log into your computer. Still need help? Contact <a href="mailto:michael@highpoweredhelp.com">support</a>.</p>
 </div> <!-- /container -->
+<?php $AppEngine->runActions('end-container'); ?>
+</div><!-- End Container -->
+<footer>
+
+<?php $AppEngine->runActions('footer-open'); ?>
+
+<?php $AppEngine->runActions('footer-close'); ?>
+
+</footer>
+
+
+<?php $AppEngine->runActions('footer-inject-js'); ?>
+</body>
+</html>
